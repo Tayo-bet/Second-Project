1f2341dd1148133f905c2ffd85a1b9ccdd5e4bd7
Tayo-bet <tayo4ayo@gmail.com>
2020-10-19 20:03:22 +0000

    first commit

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $.instagramFeed({
+            'username': 'glorytabernacleinternational',
+            'container': "#instagram-feed",
+            'display_profile':false,
+            'display_biography':false,
+            'display_igtv':false,
+            'items': 10,
+            'items_per_row': 2
+          });
+        
+});
